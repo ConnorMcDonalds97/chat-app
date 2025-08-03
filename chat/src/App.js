5b2d614f10b9f09d980f89b58a1d419b401b9ef1
kevin <kcao36@outlook.com>
2025-08-03 17:59:30 -0600

    layout done

---
+++ chat/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+
+      <div className="userList">
+        <h1>User List</h1>
+      </div>
+
+      <div className="chatBoxes">
+        <div className="chat">
+          <h1>Chat</h1>
+        </div>
+        <div className="inputBox">
+          <p>Enter message here</p>
+        </div>
+      </div>
+
+    </div>
+    
+  );
+}
+
+export default App;

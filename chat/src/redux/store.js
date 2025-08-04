3b7125f3a7aac2311b70e9a930e303132578efa0
kevin <kcao36@outlook.com>
2025-08-04 17:51:58 -0600

    forgot to save smth lol

--- chat/src/redux/store.js
+++ chat/src/redux/store.js
@@ -1,9 +1,12 @@
 import { createStore } from 'redux';
 import chatReducer from './reducers';
 
+// the store holds the app's state
+
+// creates the Redux store
 const store = createStore(
-    chatReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    chatReducer, // pass the main reducer for the app to the function
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // enables debugging tools
 );
 
 export default store;

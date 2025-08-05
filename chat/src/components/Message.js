c1bdbbc0c257e439acf1a2d3fb9f653d1cd61d53
kevin <kcao36@outlook.com>
2025-08-05 00:19:59 -0600

    add components

---
+++ chat/src/components/Message.js
@@ -0,0 +1,15 @@
+// Implements the text field
+
+import React from "react";
+import PropTypes from 'prop-types'
+
+const Message = ({message, author}) => {
+    <p>
+        <i>{author}</i>; {message}
+    </p>
+}
+
+Message.PropTypes = {
+    message: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired    
+}
